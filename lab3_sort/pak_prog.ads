0d5f0a2cafb53a190985fcef4feedeff5ff0d01a
karol.witaszczyk@gmail.com <karol.witaszczyk@gmail.com>
2014-10-21 15:48:18 +0200

    rm copied files

--- lab3_sort/pak_prog.ads
+++
@@ -1,9 +0,0 @@
-package pak_prog is
-
-type Wektor is array (Integer range <>) of Float;
-
-procedure put_wektor    (W: Wektor; Kom: String);
-procedure losuj_wektor  (W: Wektor);
-procedure sortuj_wektor (W: Wektor);
-
-end pak_prog;
